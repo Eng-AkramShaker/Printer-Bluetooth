a9dc56544c478c1bca027d14b9702bdaf38f551a
Eng-AkramShaker <akramaboubakersh@gmail.com>
2023-07-24 18:57:58 +0200

    22

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'dart:io';
 
 import 'package:ams_printer/printer_manager.dart';
@@ -47,8 +49,6 @@ class _PrintersViewState extends State<PrintersView> {
     _initImg();
   }
 
-
-
   _startScan() async {
     setState(() {
       isScanning = true;
@@ -56,7 +56,7 @@ class _PrintersViewState extends State<PrintersView> {
     await _scanBluetooth.startScan();
 
     _scanBluetooth.devices.listen((dev) {
-      if(! _isDeviceAdded(dev)){
+      if (!_isDeviceAdded(dev)) {
         setState(() {
           _devices.add(dev);
         });
@@ -75,7 +75,6 @@ class _PrintersViewState extends State<PrintersView> {
   }
 
   bool _isDeviceAdded(BluetoothDevice device) => _devices.contains(device);
-
 
   _initImg() async {
     try {
@@ -98,81 +97,111 @@ class _PrintersViewState extends State<PrintersView> {
       ),
       body: Column(
         children: [
-          const SizedBox(
-            height: 15,
-          ),
+          const SizedBox(height: 100),
+
+          // ---------------------------------------------------------------------------------------------------
           ConditionalBuilder(
-            condition: !isScanning,
-            builder: (context) => FloatingActionButton(
-              onPressed: () {
-                _startScan();
-              },
-              child: const Icon(
-                Icons.bluetooth_audio,
-              ),
-            ),
-            fallback: (context) => const CircularProgressIndicator(
-              color: Colors.lightBlue,
-            ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          ConditionalBuilder(
-              condition: _selectedDevice != null,
-              builder: (context) => Column(
+            condition: _selectedDevice != null,
+            builder: (context) => Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(10),
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 175, 224, 177),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
                     children: [
-                      _buildDev(_selectedDevice!),
-                      const SizedBox(height: 15),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          MaterialButton(
-                            onPressed: () {
-                              PrinterManager.printImg(imgFile.path);
-                            }, color: Colors.lightBlue,
-                            child: const Text(
-                              "Print",
-                              style: TextStyle(color: Colors.white),),
-                          ),
-                          MaterialButton(
-                            onPressed: () {
-                              PrinterManager.connect(_selectedDevice!.address);
-                            }, color: Colors.lightBlue,
-                            child: const Text(
-                              "Connect",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                        ],
-                      )
+                      Text(_selectedDevice!.name,
+                          style: const TextStyle(
+                              color: Colors.lightBlue,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold)),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(_selectedDevice!.address,
+                          style:
+                              const TextStyle(color: Colors.grey, fontSize: 14))
                     ],
                   ),
-              fallback: (context) => Container(
-                    child: const Text("لم يتم تحديد طابعة"),
-                  )),
+                ),
+                const SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    MaterialButton(
+                      onPressed: () {
+                        PrinterManager.printImg(imgFile.path);
+                      },
+                      color: Colors.lightBlue,
+                      child: const Text(
+                        "إطبع",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    MaterialButton(
+                      onPressed: () {
+                        PrinterManager.connect(_selectedDevice!.address);
+                      },
+                      color: Colors.lightBlue,
+                      child: const Text(
+                        "أتصال",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            fallback: (context) => Container(
+              child: const Text("لم يتم تحديد طابعة"),
+            ),
+          ),
+
+          // ---------------------------------------------------------------------------------------------------
+
           const SizedBox(
             height: 15,
           ),
           Container(
-            height: 1,
+            height: 7,
             width: double.infinity,
-            color: Colors.lightBlue,
+            color: Colors.red,
           ),
           const SizedBox(
             height: 15,
           ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  ..._devices.map(
-                    (dev) => _buildDev(dev),
-                  )
-                ],
-              ),
-            ),
-          ),
+
+          // =======================================================================================================
+
+          ElevatedButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  builder: (context) {
+                    return SizedBox(
+                      height: 700,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ..._devices.map((dev) => _buildDev(dev)),
+                        ],
+                      ),
+                    );
+                  });
+            },
+            child: const Text("بحث"),
+          )
         ],
       ),
     );
@@ -182,6 +211,7 @@ class _PrintersViewState extends State<PrintersView> {
         onTap: () {
           setState(() {
             _selectedDevice = dev;
+            Navigator.pop(context);
           });
         },
         child: Container(
@@ -189,7 +219,7 @@ class _PrintersViewState extends State<PrintersView> {
           padding: const EdgeInsets.all(10),
           margin: const EdgeInsets.all(10),
           decoration: BoxDecoration(
-            color: Colors.grey.withOpacity(0.125),
+            color: const Color.fromARGB(255, 175, 224, 177),
             borderRadius: BorderRadius.circular(20),
           ),
           child: Column(
